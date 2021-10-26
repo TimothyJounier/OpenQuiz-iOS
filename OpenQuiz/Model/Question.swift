05ce8efe71f8a06878c2834499f717a81a776854
Timothy <timothyjounier@gmail.com>
2021-10-26 14:06:40 +0200

    Initial Commit

---
+++ OpenQuiz/Model/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  OpenQuiz
+//
+//  Created by Timothy jounier on 26/10/2021.
+//
+
+import Foundation
+
+struct Question {
+    var title = ""
+    var isCorrect = false
+}
